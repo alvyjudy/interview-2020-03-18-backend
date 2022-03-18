ff311e68e0150a957b48785d43ce323eac16f034
Di Zhu <dzhu@onekingslane.com>
2022-03-17 23:19:50 -0400

    add sumary

--- myapp/myapp/views.py
+++ myapp/myapp/views.py
@@ -1,3 +1,4 @@
+import re
 import json
 import csv
 from io import StringIO
@@ -20,24 +21,48 @@ def index(request):
         json.dump(list(specs), f, indent=2)
     
     plans = generate_plans(filepath)
-    print(plans)
 
-    with open(f'{plans}/plan_1.json') as file:
-        plan1 = json.load(file)
+    best_plan = None
+    highest_score = 0
+    for filename in os.listdir(plans):
+        with open(f'{plans}/{filename}') as file:
+            plan = json.load(file)
+            score = score_plan(plan) or 0
+            if score > highest_score:
+                best_plan = plan
+                highest_score = score
+
+    [summaryA, summaryB] = map(lambda plan: sum_usage(plan), [plan['serverA'], plan['serverB']])
     
-    best_plan = plan1
-    score = score_plan(plan1)
-
     return JsonResponse({
         "best_plan": best_plan,
-        "score": score,
-        "server_a_summary": {
-            "total_cpu": '',
-            "total_ram": "",
-            "total_disk_usage": "",
-        },
-        "server_b_summary": {
-
-        }
+        "score": highest_score,
+        "server_a_summary": summaryA,
+        "server_b_summary": summaryB,
     })
 
+def sum_usage(vms):
+    total_memory = 0
+    total_disk = 0
+    for vm in vms:
+        (vm_memory, _) = parse(vm['memory'])
+        (vm_disk, _) = parse(vm['disk'])
+        total_memory += vm_memory
+        total_disk += vm_disk
+
+    return {
+        "cpu_cores": len(vms),
+        "memory": f'{total_memory}Mi',
+        "disk": f'{total_disk}Gb',
+    }
+
+def parse(text):
+    (number, unit) = re.match('([0-9]+)([A-Za-z]+)', text).group(1, 2)
+    number = int(number)
+    if unit == 'Gi':
+        return (number * 1024, 'Mi')
+    if unit == 'Ti':
+        return (number * 1024, 'Gb')
+
+    return (number, unit)
+    
